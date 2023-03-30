caed058420eeaba014c8308daf34b03d0999e839
88870 <121027247+88870@users.noreply.github.com>
2023-03-30 11:02:04 +0200

    Add files via upload

---
+++ infjs.js
@@ -0,0 +1,45 @@
+document.getElementById("foto1").onclick = function() {myFunction()};
+
+function myFunction() {
+    let f1= document.getElementById("foto1");
+let t1=document.getElementById("tek");
+
+    if(f1.onclick)
+document.getElementById("tek").innerHTML="<h2>Naam1</h2>Wat hebben ze gedaan:<ul><li>...</li><li>...</li><li>...</li><li>...</li></ul> ";
+  }
+
+
+
+  document.getElementById("foto2").onclick = function() {myFunction2()};
+
+  function myFunction2() {
+    let f2= document.getElementById("foto2");
+let t1=document.getElementById("tek");
+
+    if(f2.onclick)
+document.getElementById("tek").innerHTML="<h2>Naam2</h2>Wat hebben ze gedaan:<ul><li>...</li><li>...</li><li>...</li><li>...</li></ul>";
+  }
+
+
+
+  document.getElementById("foto3").onclick = function() {myFunction3()};
+
+  function myFunction3() {
+    let f3= document.getElementById("foto3");
+let t1=document.getElementById("tek");
+
+    if(f3.onclick)
+document.getElementById("tek").innerHTML="<h2>Naam3</h2>Wat hebben ze gedaan:<ul><li>...</li><li>...</li><li>...</li><li>...</li></ul>";
+  }
+
+
+
+  document.getElementById("foto4").onclick = function() {myFunction4()};
+
+function myFunction4() {
+    let f4= document.getElementById("foto4");
+let t1=document.getElementById("tek");
+
+    if(f4.onclick)
+document.getElementById("tek").innerHTML="<h2>Naam4</h2>Wat hebben ze gedaan:<ul><li>...</li><li>...</li><li>...</li><li>...</li></ul> ";
+  }
